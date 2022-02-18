e7753b8c89afebabc250708e6dce3da7d05103ee
Colleen Lemak <clemak@zagmail.gonzaga.edu>
2022-02-18 12:48:17 +0100

    commit

--- Alpha-Beta Tree/tree_shenanigans.py
+++ Alpha-Beta Tree/tree_shenanigans.py
@@ -1,38 +1,58 @@
-
+'''
+Name: Colleen Lemak
+Class: CSCI 4740
+Professor: Jorge Martinez
+Date: 17 February 2022
+'''
 import math
 
-def print_perfect_binary_tree(scores, cur_depth, node_idx, tree_depth):
-    if cur_depth == tree_depth:
-        print(scores[node_idx], end=", ")
-    else:
-        left_child = 2 * node_idx + 0
-        right_child = 2 * node_idx + 1
-        
-        print_perfect_binary_tree(scores, cur_depth + 1, left_child, tree_depth)
-        print_perfect_binary_tree(scores, cur_depth + 1, right_child, tree_depth)
-
-# when a leaf is reached, it returns the score (the depth of the leaves is equal to tree_depth)
-def minimax(depth, node, maximize, scores, tree_depth):
+def minimax(depth, node, maximize, scores, tree_depth, verbose=False):        
+    # when a leaf is reached, it returns the score (the depth of the leaves is equal to tree_depth)
+    
     if depth == tree_depth:
-        print("Evaluating score ", scores[node])
+        if verbose:
+            print ("Evaluating score ", scores[node])
+            
         return (scores[node])
+        
     else:
-        # if not a leaf, min or max the score
+        # if the node is not a leaf, minimize or maximize the score of its children
+
         left_child = node * 2 + 0
         right_child = node * 2 + 1
         
-        if(maximize):
-            return max(minimax(depth + 1, left_child, False, scores, tree_depth), minimax(depth + 1, right_child, False, scores, tree_depth))
+        if maximize:
+            # returns the max score of its children
+                        
+            return max(minimax(depth + 1, left_child, False, scores, tree_depth, verbose), minimax(depth + 1, right_child, False, scores, tree_depth, verbose))
+     
         else:
-            return min(minimax(depth + 1, left_child, False, scores, tree_depth), minimax(depth + 1, right_child, False, scores, tree_depth))
-        
+            # returns the min score of its children
+
+            return min(minimax(depth + 1, left_child, True, scores, tree_depth, verbose), minimax(depth + 1, right_child, True, scores, tree_depth, verbose)) 
 
 
 if __name__ == '__main__':
+
+    # the search tree is modeled using a perfect binary tree having 2, 4, 8, 16, 32 leaves 
+    # the value of each leaf is the score of the game, these values are stored in a list
+    
     scores = [2, 4, 5, 10, 1, 3, 1, 0]
+
+    # the tree depth is log2(total leaves)
+    
     tree_depth = math.log(len(scores), 2)
-    
-    # print_perfect_binary_tree(scores, 0, 0, 3)
-    
-    print("The optimal value for a Max player is ", minimax(0, 0, True, scores, tree_depth))
-    print("The optimal value for a Min player is ", minimax(0, 0, False, scores, tree_depth))
+
+    # minimax
+
+    print ("\nMinimax \n")
+
+    print ("The optimal value for a Max player is", minimax(0, 0, True, scores, tree_depth))
+    print ("The optimal value for a Min player is", minimax(0, 0, False, scores, tree_depth))
+
+    # minimax showing the leaves evaluated during the search
+
+    print ("\nMinimax showing the leaves evaluated during the search\n")
+
+    print ("The optimal value for a Max player is", minimax(0, 0, True, scores, tree_depth, True))
+    print ("The optimal value for a Min player is", minimax(0, 0, False, scores, tree_depth, True))
