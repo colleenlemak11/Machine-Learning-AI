229eee73add4a56576ee0b5d7803dcec5ebc7cda
Colleen Lemak <clemak@zagmail.gonzaga.edu>
2022-03-16 12:26:50 +0100

    commit

--- TicTacToe/tictactaoeAI.py
+++ TicTacToe/tictactaoeAI.py
@@ -8,6 +8,7 @@ import math
 
 # Defining TicTacToe Class
 class TicTacToe:
+    
     def __init__(self):
         self.grid = [['_','_','_'],
                      ['_','_','_'],
@@ -75,141 +76,153 @@ class TicTacToe:
 
 
     def playAI(self):
-            while True:
-                self.draw()
-                
-                winner = self.game_over()
-
-                # if the game is over
-                
-                if winner != None:
-                    if winner == 'X':
-                        print ("X player wins!")
-                    elif winner == 'O':
-                        print ("O player wins!")
-                    elif winner == '_':
-                        print ("The game ends with a forced draw!")
-                    
-                    return
-
-                # if it is the X player turn -- Human Player
-                
-                if self.player_turn == 'X':
-                    while True:
-                        print ("Player X enter coordinates x y: ", end="")
-
-                        px, py = map(int, input().split())
-
-                        if self.valid_coordinates(px, py):
-                            self.grid[px][py] = 'X'
-                            self.player_turn = 'O'
-                            break
-                        else:
-                            print ("Coordinates are not valid! Play again")
-
-                # if it is the O player turn -- AI Player
-                
-                else:
-                    alpha = MINUS_INFINITE
-                    beta = PLUS_INFINITE
-                    while True:
-                        print("AI Player is making a decision...")
-                        
-                        value, px, py = minimax_min(self.grid, winner, alpha, beta)
-
-                        if self.valid_coordinates(px, py):
-                            self.grid[px][py] = 'O'
-                            self.player_turn = 'X'
-                            break
-                        else:
-                            print ("Coordinates are not valid! Play again")
-
-
-
-# Defining Minimax Algorithm with Alpha-Beta Pruning
-
-PLUS_INFINITE  = +1000
-MINUS_INFINITE = -1000
- 
-def minimax_min(grid, winner, alpha, beta):
-    # Human Player (X) is a Min Player, possible values for the Min
-    # are -1=win, 0=draw, +1=loss, initial value is +2
-    min_value = +2
-    
-    # min_px, min_py are the coordinates of the optimal play
-    min_px = -1
-    min_py = -1
-    
-    # if game is over, return the winner player
-    if winner == 'X':
-        return(-1,0,0) # X is Min Player, its best play is -1
-    elif winner == 'O':
-        return(1,0,0) # O is Max Player, its best play is +1
-    elif winner == '_':
-        return(0,0,0) # draw
-    
-    # if game is not over, evaluate all possible plays
-    for i in [0,1,2]:
-        for j in [0,1,2]:
-            # if coordinate [i][j] is empty, evaluate the play for the X player
-            if grid[i][j] == '_':
-                grid[i][j] == 'X'
-                
-                beta = min(beta, min_value)
-                # minimax returns a tuple with the value of a final state
-                (value, max_i, max_j) = minimax_max(grid, winner, alpha, beta)
-                
-                if value < min_value:
-                    min_value = value;
-                    min_px = i
-                    min_py = j
-                    
-                grid[i][j] = '_'
-    
-    return (min_value, min_px, min_py)
         
+        while True:
+            self.draw()
+            winner = self.game_over()
 
-def minimax_max(grid, winner, alpha, beta):
-    # the AI Player is O, and is a max player, possible values for max
-    # are -1=loss, 0=draw, +1=win, initial value is -2
-    max_value = -2
-    
-    # max_px, max_py are coodinates of the optimal play
-    max_px = -1
-    max_py = -1
-    
-    # check if game is over and return winner player
-    if winner == 'X':
-        return(-1,0,0)
-    elif winner == 'O':
-        return(1,0,0)
-    elif winner == '_':
-        return(0,0,0)
-    
-    # if game is not over, evaluate all possible plays
-    for i in [0,1,2]:
-        for j in [0,1,2]:
-            # if coordinate [i][j] is empty, evaluate the play for the O player
-            if grid[i][j] == '_':
-                grid[i][j] == 'O'
-                
-                alpha = max(alpha, max_value)
-                # minimax_min returns a tuple with the value of a final state
-                (value, min_i, min_j) = minimax_min(grid, winner, alpha, beta)
-                
-                # max_value is updated when a greater value is found
-                if value > max_value:
-                    max_value = value;
-                    max_px = i;
-                    max_py = j
+            # if the game is over
+            if winner != None:
+                if winner == 'X':
+                    print ("X player wins!")
+                elif winner == 'O':
+                    print ("O player wins!")
+                elif winner == '_':
+                    print ("The game ends with a forced draw!")
                     
-                # when the recursive calls for the [i][j] play terminate
-                # the value of [i][j] is restored
-                grid[i][j] = '_'
-                
-    # minimax returns the value and the coordinates of the optimal play
-    return (max_value, max_px, max_py)
+                return
 
+            # if it is the X player turn -- Human Player
+            if self.player_turn == 'X':
+                while True:
+                    print ("Player X enter coordinates x y: ", end="")
+
+                    px, py = map(int, input().split())
+
+                    if self.valid_coordinates(px, py):
+                        self.grid[px][py] = 'X'
+                        self.player_turn = 'O'
+                        break
+                    else:
+                        print ("Coordinates are not valid! Play again")
+
+            # if it is the O player turn -- AI Player
+            
+            else: 
+                (value, px, py) = self.minimax_max(-2, 2)
+                                
+                self.grid[px][py] = 'O'
+                self.player_turn = 'X'
+                
+            
+                
+
+
+    # Defining Minimax Algorithm with Alpha-Beta Pruning
+    def minimax_min(self, alpha, beta): 
+        # the human player (X) is a Min player, possible values for Min player are -1: win, 0: draw, +1: loss, its initial value is +2    
+
+        min_value = 2
+
+        # min_px, min_py are the coordinates of the optimal play
+        
+        min_px = -1
+        min_py = -1
+
+        winner = self.game_over()
+
+        # if the game is over, return -1, 0, +1
+        
+        if winner == 'X':
+            return (-1, 0, 0)  # X is a Min player, its best play has value -1
+        elif winner == 'O':
+            return (1, 0, 0)   # O is a Max player, its best play has value +1
+        elif winner == '_':
+            return (0, 0, 0)   # the game ends with a draw
+
+        # evaluate all possible plays
+        
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if self.grid[i][j] == '_':
+                    self.grid[i][j] = 'X'
+
+                    # minimax_max returns a tuple with the value of a final state
+                    
+                    (value, max_i, max_j) = self.minimax_max(alpha, beta)
+
+                    # min_value is updated when a smaller value is found
+                                        
+                    if value < min_value:
+                        min_value = value
+                        min_px = i
+                        min_py = j
+
+                    # when the recursive calls for the [i][j] play terminate, the value of [i][j] is restored
+                                        
+                    self.grid[i][j] = '_'
+                    
+                    if min_value <= alpha:
+                        return (min_value, min_px, min_py)
+                    if min_value < beta:
+                        beta = min_value
+                        
+        # minimax_min returns the min_value and the coordinates min_px, min_py of the optimal play
+        return (min_value, min_px, min_py)
+
+
+
+
+    def minimax_max(self, alpha, beta): 
+        # the AI player (O) is a Max player, possible values for the Max player are -1: loss, 0: draw, +1: win, its initial value is -2
+        
+        max_value = -2
+
+        # max_px, max_py are the coordinates of the optimal play
+        
+        max_px = -1
+        max_py = -1
+
+        winner = self.game_over()
+
+        # if the game is over, return -1, 0, +1
+        
+        if winner == 'X':
+            return (-1, 0, 0)  # X is a Min player, its best play has value -1
+        elif winner == 'O':
+            return (1, 0, 0)   # O is a Max player, its best play has value +1
+        elif winner == '_':
+            return (0, 0, 0)   # the game ends with a draw
+
+        # evaluate all possible plays
+
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if self.grid[i][j] == '_':
+                    self.grid[i][j] = 'O'
+
+                    # minimax_min returns a tuple with the value of a final state
+                    
+                    (value, min_i, min_j) = self.minimax_min(alpha, beta)
+
+                    # max_value is updated when a greater value is found
+                    
+                    if value > max_value:
+                        max_value = value
+                        max_px = i
+                        max_py = j
+
+                    # when the recursive calls for the [i][j] play terminate, the value of [i][j] is restored
+                    
+                    self.grid[i][j] = '_'
+                    
+                    if max_value >= beta:
+                        return (max_value, max_px, max_py)
+                    if max_value > alpha:
+                        alpha = max_value
+                        
+        # minimax_max returns the max_value and the coordinates max_px, max_py of the optimal play
+        return (max_value, max_px, max_py)
 
 
 
